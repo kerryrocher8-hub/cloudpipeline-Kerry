bac0086a8c74fbbde5577108d9c76dfe3ce53983
kerryrocher8-hub <kerryrocher8@gmail.com>
2025-11-09 10:02:22 -0500

    Add Terraform variables for project configuration

--- iac/iac/variables.tf
+++ iac/iac/variables.tf
@@ -1,1 +1,16 @@
+variable "project_name" {
+  default = "cloudpipeline-kerry"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
 
